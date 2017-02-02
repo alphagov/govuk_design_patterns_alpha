ed9e14b59862985c8f5e19d56f41b5a351a9298a
Tim Paul <contact@timpaul.co.uk>
2017-02-02 13:19:41 +0000

    Initial commit

---
+++ source/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require vendor/jquery
+//= require vendor/modernizr
+//= require vendor/fixedsticky
+//= require vendor/lodash
+//= require analytics
+//= require header
+//= require start-modules
+
+$(function() {
+  $('.fixedsticky').fixedsticky();
+});
---
+++ source/javascripts/start-modules.js
@@ -0,0 +1,8 @@
+//= require govuk/modules
+//= require modules/anchored-headings
+//= require modules/in-page-navigation
+//= require modules/table-of-contents
+
+$(document).ready(function() {
+  GOVUK.modules.start();
+});
